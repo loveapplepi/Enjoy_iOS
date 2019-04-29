e5257f2e80281bf1898553397fbb7cc0b940b62d
loveapplepi <loveapplepi@naver.com>
2019-04-29 14:52:27 +0900

    make mapkit

--- MapkitTutorial/MapkitTutorial/ViewController.swift
+++ MapkitTutorial/MapkitTutorial/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let distance: CLLocationDistance = 800
+    let pitch: CGFloat = 400
+    let heading = 45.0
+    var camera: MKMapCamera?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapView.mapType = .satelliteFlyover
+        let coordinate = CLLocationCoordinate2DMake(38.889819, -77.009066)
+        camera = MKMapCamera(lookingAtCenter: coordinate, fromDistance: distance, pitch: pitch, heading: heading)
+        mapView.camera = camera!
+        
     }
 
-
+    @IBAction func rotateLeft(_ sender: Any) {
+    }
+    
+    @IBAction func rotateRight(_ sender: Any) {
+    }
 }
 
